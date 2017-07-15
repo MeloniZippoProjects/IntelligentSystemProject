83b508a060a497e067be67e1c2ed54a455f7aefa
rzippo <raffaele.zippo@live.it>
2017-07-15 12:17:04 +0200

    Pre merge commit

---
+++ IntegrationTest.m
@@ -0,0 +1,38 @@
+clear;
+load('.\restructured_dataset\dati_buoni\data.mat');
+
+signalIdx = 1;
+
+powerSignal = data.dynamic.H.I.phase{signalIdx};
+timeStamps = data.dynamic.H.I.timestamps{signalIdx};
+
+fig = figure('Name', 'Original power signal');
+plot(powerSignal);
+
+normalizedPowerSignal = (powerSignal-mean(powerSignal))/std(powerSignal);
+
+fig = figure('Name', 'Normalized power signal');
+plot(normalizedPowerSignal);
+
+timeSlices = 12;
+n = size(powerSignal, 1);
+sliceSize = floor(n/timeSlices);
+
+integrationVector = zeros(timeSlices,1);
+
+timeDistances = zeros(n, 1);
+timeDistances(1:n-1) = timeStamps(2:n) - timeStamps(1:n-1);
+timeDistances(n) = timeDistances(n-1);
+
+for timeSlice = 1:timeSlices
+    startIdx = (timeSlice - 1)*sliceSize + 1;
+    endIdx = timeSlice*sliceSize;
+    integrationVector(timeSlice) = sum( normalizedPowerSignal(startIdx:endIdx).*timeDistances(startIdx:endIdx) );
+end
+
+fig = figure('Name', 'Sliced integral');
+plot(integrationVector);
+
+normalizedIntegrationVector = (integrationVector - mean(integrationVector)) / std(integrationVector);
+fig = figure('Name', 'Normalized sliced integral');
+plot(normalizedIntegrationVector);
---
+++ samplingFactorTesting.m
@@ -0,0 +1,14 @@
+maxSamples = 12;
+maxNeurons = 10;
+
+for sampleIdx = 5 : maxSamples
+    
+   for neurons = 1 : maxNeurons
+      trainingSetSliceBuilder_IvUvP
+       
+      net = patternnet(neurons); 
+       
+   end
+    
+    
+end
