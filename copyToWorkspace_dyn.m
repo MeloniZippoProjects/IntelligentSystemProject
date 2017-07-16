0c9e95a7621430011e008235cb0ddbcd3f55261f
rzippo <raffaele.zippo@live.it>
2017-07-16 17:39:05 +0200

    Working on anfis

--- copyToWorkspace_dyn.m
+++ copyToWorkspace_dyn.m
@@ -1,29 +1,29 @@
-trainTable = load('features_dataset/dati_buoni/trainingSlicedSet_H.mat');
+trainTable = load('features_dataset/dati_buoni/trainingSlicedSet_V.mat');
 
 
 trainInput = [trainTable.table.power_integral];
 trainInput = trainInput(31:60,:);
 trainOutput = [trainTable.table.out_dynamicCase];
-trainOutput = trainOutput(31:60,:);
+%trainOutput = trainOutput(31:60,:);
 
+trainTableData(:,1:3) = trainInput(randperm(30),:);
+trainTableData(1:10,4) = 0;
+trainTableData(11:21,4) = 1;
+trainTableData(21:30,4) = 0.5;
 
+trainRandInput(:,1:4) = trainTableData(randperm(30),:); 
 
-trainData = zeros(24,4);
-trainData(1:8,1:3) = trainInput(1:8,:);
-trainData(1:8,4) = -1;
-
-trainData(9:16,1:3) = trainInput(11:18,:);
-trainData(9:16,4) = 0;
-
-trainData(17:24,1:3) = trainInput(21:28,:);
-trainData(17:24,4) = 1;
+trainData = zeros(18,4);
+trainData(1:6,1:4) = trainRandInput(1:6,:);
+trainData(7:12,1:4) = trainRandInput(11:16,:);
+trainData(13:18,1:4) = trainRandInput(21:26,:);
 
 testData = zeros(6,4);
-testData(1:2,1:3) = trainInput(9:10,:);
-testData(1:2,4) = -1;
+testData(1:2,1:4) = trainRandInput(7:8,:);
+testData(3:4,1:4) = trainRandInput(17:18,:);
+testData(5:6,1:4) = trainRandInput(27:28,:);
 
-testData(3:4,1:3) = trainInput(19:20,:);
-testData(3:4,4) = 0;
-
-testData(5:6,1:3) = trainInput(29:30,:);
-testData(5:6,4) = 1;
+checkData = zeros(6,4);
+checkData(1:2,1:4) = trainRandInput(9:10,:);
+checkData(3:4,1:4) = trainRandInput(9:10,:);
+checkData(5:6,1:4) = trainRandInput(9:10,:);
