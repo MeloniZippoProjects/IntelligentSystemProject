11c700e665f2a83639a443b453847e63cb9f95d7
rzippo <raffaele.zippo@live.it>
2017-07-16 13:52:12 +0200

    Add results anfis

--- copyToWorkspace_SvD.m
+++ copyToWorkspace_SvD.m
@@ -1,4 +1,15 @@
-trainTable = load('features_dataset/dati_buoni/trainingSet_SvD_V.mat');
+trainTable = load('features_dataset/dati_buoni/trainingSet_SvD_H.mat');
 normalizedStd = normalize(trainTable.table.phase_std);
-trainInput = normalizedStd.values;
-trainOutput = [trainTable.table.out_isStatic];
+
+trainData = zeros(50, 2);
+trainData(1:25, 1) = normalizedStd.values(1:25);
+trainData(26:50, 1) = normalizedStd.values(36:60);
+trainData(1:25, 2) = trainTable.table.out_isStatic(1:25);
+trainData(26:50, 2) = trainTable.table.out_isStatic(36:60);
+
+testingData = zeros(10, 2);
+testingData(:, 1) = normalizedStd.values(26:35);
+testingData(:, 2) = trainTable.table.out_isStatic(26:35);
+
+%trainInput = normalizedStd.values;
+%trainOutput = [trainTable.table.out_isStatic];
--- copyToWorkspace_dyn.m
+++ copyToWorkspace_dyn.m
@@ -1,7 +1,29 @@
-trainTable = load('features_dataset/dati_buoni/trainingSlicedSet_V.mat');
+trainTable = load('features_dataset/dati_buoni/trainingSlicedSet_H.mat');
 
 
 trainInput = [trainTable.table.power_integral];
 trainInput = trainInput(31:60,:);
 trainOutput = [trainTable.table.out_dynamicCase];
 trainOutput = trainOutput(31:60,:);
+
+
+
+trainData = zeros(24,4);
+trainData(1:8,1:3) = trainInput(1:8,:);
+trainData(1:8,4) = -1;
+
+trainData(9:16,1:3) = trainInput(11:18,:);
+trainData(9:16,4) = 0;
+
+trainData(17:24,1:3) = trainInput(21:28,:);
+trainData(17:24,4) = 1;
+
+testData = zeros(6,4);
+testData(1:2,1:3) = trainInput(9:10,:);
+testData(1:2,4) = -1;
+
+testData(3:4,1:3) = trainInput(19:20,:);
+testData(3:4,4) = 0;
+
+testData(5:6,1:3) = trainInput(29:30,:);
+testData(5:6,4) = 1;
